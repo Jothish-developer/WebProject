efa874c57252d31299158c8b628262dc273fb7e1
Jothish-developer <ajothish150703@gmail.com>
2025-04-27 14:56:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('button');
+const check =document.getElementById('Check')
+button.addEventListener('click', ()=>
+{
+    alert('Please Wait...');
+})
+
+check.addEventListener('click',()=>
+{
+   alert('Please check with Home Page.../')
+})
